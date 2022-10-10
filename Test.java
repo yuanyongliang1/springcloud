9db35d8d41386bf719a6a3af6b73424fb6a57de5
yuanyongliang1 <1513503916@qq.com>
2022-10-10 19:06:23 +0800

    Revert "第er 次"
    This reverts commit 47ef0c4f1b77628791deff1c49454ca688c740e5.

--- Test.java
+++ Test.java
@@ -2,6 +2,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("第一次提交");
-        System.out.println("第er 次提交");
     }
 }
