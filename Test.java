bf5e5c1e3f79185dd808a7e44a8d0c47afb4c6b9
yuanyongliang1 <1513503916@qq.com>
2022-10-11 16:48:43 +0800

    diwuci

--- Test.java
+++ Test.java
@@ -1,7 +1,13 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("第一次提交");
         System.out.println("第er 次提交3");
+        new Date();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(new Date());
     }
 }
